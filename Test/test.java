aa8afe0fa3001ed9b63fe1554ddfe6e45d7952ab
AVijn <arjan.witharen@gmail.com>
2022-12-02 11:14:53 +0100

    UnitTest test

---
+++ Test/test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class test {
+
+    String string = "test";
+    int num = 1;
+
+    @Test
+    void isNumber()
+    {
+        assertEquals(1, this.num);
+    }
+}
