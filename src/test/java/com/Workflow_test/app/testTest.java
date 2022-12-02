778b114ac5ef4a35d4d60918cbf2958f5bbae923
AVijn <arjan.witharen@gmail.com>
2022-12-02 15:25:04 +0100

    ja

---
+++ src/test/java/com/Workflow_test/app/testTest.java
@@ -0,0 +1,12 @@
+package com.Workflow_test.app;
+
+import org.junit.jupiter.api.*;
+
+public class testTest {
+
+    @Test
+    void isNumber()
+    {
+        System.out.println("Greetings");
+    }
+}
